ae78d7a845a58d91fc2d29f2dde0403b3530764a
DerrickStew <ds283548@email.vccs.edu>
2024-01-30 16:26:51 -0500

    Updated dateType, having errors (everything is invalid date)

--- dateType.cpp
+++ dateType.cpp
@@ -1,1 +1,82 @@
 #include "dateType.h"
+#include <iostream>
+
+using namespace std;
+
+void dateType::print()
+{
+	cout << dMonth << "-" << dDay << "-" << dYear << endl;
+}
+
+dateType::dateType(int month, int day, int year)
+{
+	// check for valid date
+	if (isDateGood(month, day, year))
+	{
+		dMonth = month;
+		dDay = day;
+		dYear = year;
+	}
+	else
+	{
+		cout << "Invalid date, default date (1-1-1900) is set";
+	}
+}
+
+void dateType::setDate(int month, int day, int year)
+{
+	// check for valid date
+	if (isDateGood(month, day, year))
+	{
+		dMonth = month;
+		dDay = day;
+		dYear = year;
+	}
+	else
+	{
+		cout << "Invalid date, default date (1-1-1900) is set";
+	}
+}
+
+bool dateType::isDateGood(int month, int day, int year)
+{
+	// check for valid year (>1900)
+	if (month < 1900)
+		return false;
+
+	// check for valid month 
+	if (month < 1 || month > 12)
+		return false;
+
+	// check for valid number a day can be in general
+	if (day < 1 || day > 31)
+		return false;
+	// check for valid number a day can be in specific month
+	if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
+		return false;
+
+	// check for leap year, february
+	if (month == 2)
+	{
+		if (isLeapYear(year))
+		{
+			if (day > 29)
+				return false;
+		}
+		else {
+			if (day > 28)
+				return false;
+		}
+	}
+	return true;
+}
+
+bool dateType::isLeapYear(int year)
+{
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+	{
+		return true;
+	}
+	else
+		return false;
+}
--- dateType.h
+++ dateType.h
@@ -1,5 +1,37 @@
 #pragma once
 class dateType
 {
+private:
+	int dMonth = 1;
+	int dDay = 1;
+	int dYear = 1900; 
+	
+	// isDateGood
+	// preconditions - supplied values are 3 integers
+	// postconditions - true is returned if date is valid and false otherwise 
+	bool isDateGood(int, int, int);
+
+public:
+
+	dateType(int = 1, int = 1, int = 1900);
+
+	// setDate
+	// preconditions - supplied values are 3 integers 
+	// postcondition - use the values supplied if valid, otherwise use the default (1,1,1900)
+	void setDate(int, int, int);
+
+	// print
+	//postcondition - date is displayed
+	void print();
+
+	// isLeapYear
+	// preconditions - supplied value is an integer
+	// postcondition - true is returned if year is a leap year and false otherwise
+	bool isLeapYear(int);
+
+	int getDay() { return dDay;  }
+	int getMonth() { return dMonth;  }
+	int getYear() { return dYear;  }
+
 };
 
